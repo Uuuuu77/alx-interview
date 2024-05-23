904adeeca2a2fcd0a9ee848661993b040d38acea
John Njuguna <johnmnjuguna89@gmail.com>
2024-05-23 10:33:09 -0700

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,42 +3,21 @@
 """ UTF-8 Validation """
 
 
-def validUTF8(data):
+def validUTF8(data) -> bool:
     """ Determines if a given data set represents a valid UTF-8 encoding """
-    def get_byte_count(first_byte):
-        if first_byte & 0b11110000 == 0b11110000:
-            return 4
-        elif first_byte & 0b11100000 == 0b11100000:
-            return 3
-        elif first_byte & 0b11100000 == 0b11000000:
-            return 2
+    no_bytes = 0
+    for byte in data:
+        mask = 1 << 7
+        if not no_bytes:
+            while byte & mask:
+                no_bytes += 1
+                mask >>= 1
+            if not no_bytes:
+                continue
+            if no_bytes == 1 or no_bytes > 4:
+                return False
         else:
-            return 1
-
-        x = 0
-        n = len(data)
-
-        while x < n:
-            current_byte = data[x]
-
-            # Number of bytes in the current UTF-8 character.
-            span = get_byte_count(current_byte)
-
-            if span == 1:
-                # Single-byte character.
-                if current_byte > 0x7F:
-                    return False
-            else:
-                # Multi-byte character.
-                if n - x < span:
-                    return False
-
-                for y in range(x + 1, x + span):
-                    if not (0x80 <= data[y] <= 0xBF):
-                        return False
-
-                x += span - 1
-
-            x += 1
-
-        return True
+            if byte >> 6 != 0b10:
+                return False
+        no_bytes -= 1
+    return no_bytes == 0
