7a728a0ccff4070ed037d55551f2e7ba9a45b252
John Njuguna <johnmnjugun89@gmail.com>
2024-06-19 07:51:16 -0700

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" Making change using a greedy algorithm """
+
+
+def makeChange(coins, total):
+    """ Returns the fewest number of coins
+        needed to meet the given total amount
+    """
+    if total < 1:
+        return 0
+
+    coins.sort(reverse=True)
+    num_coins = 0
+
+    for coin in coins:
+        num_coins += total // coin
+        total %= coin
+
+    return num_coins if total == 0 else -1
